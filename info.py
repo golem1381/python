962f65a328d9a38cad407af476c05857b0c40e36
golem1381 <pa.alemiii81@gmail.com>
2016-06-24 22:27:29 +0430

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,12 @@ bot = telebot.TeleBot("TOKEN")
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(m):
-    bot.reply_to(m, "Hello I'm ID bot \n\n Send : \n  /id or /me or /info   \n or all pm text \n\n get your id : \n /idme (just pv)")
+    bot.reply_to(m, "Hello.just send me any thing and see what will you see ...")
 
+@bot.message_handler(commands=['wtf', 'wtf'])
+def welcome(m):
+    bot.reply_to(m, "be to che ?")
+    
 @bot.message_handler(commands=['id', 'ids', 'info', 'me'])
 def id(m):      # info menu
     cid = m.chat.id
@@ -35,14 +39,14 @@ def id(m):      # info menu
 def c(m):
     uid = m.chat.id
     bot.send_chat_action(uid, 'typing')
-    bot.send_contact(uid, phone_number="+989216089692", first_name="alfroshotak")
+    bot.send_contact(uid, phone_number="+989398391927", first_name="parsa")
 
 
 @bot.message_handler(commands=['about']) # copy right Taylor Team
 def p(m):
     uid = m.chat.id
     bot.send_chat_action(uid, 'typing')
-    bot.send_message(uid, "Taylor Team development Telegram bot and web mastering \n\n developers : \n [negative](https://telegram.me/negative_officiall) \n [Parham](https://telegram.me/UnFriendlly)", parse_mode="Markdown")
+    bot.send_message(uid, "base by amir negative(taylor team)", parse_mode="Markdown")
     bot.send_photo(uid, open('taylor.jpg'), caption="@Taylor_Team")
 
 @bot.message_handler(func=lambda m: m.chat.type == 'private', commands=['idme'])
